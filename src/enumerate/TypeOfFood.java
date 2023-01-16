7baf71010dd5c213635e73359dbddc035b2bc68d
always <961500367@qq.com>
2023-01-16 23:26:20 +0800

    add thinking in java enum sample 20230106

---
+++ src/enumerate/TypeOfFood.java
@@ -0,0 +1,12 @@
+package enumerate;
+
+public class TypeOfFood {
+
+    public static void main(String[] args) {
+        Food food = Food.Appetizer.SALAD;
+        food = Food.MainCourse.LASAGNE;
+        food = Food.Dessert.GELATO;
+        food = Food.Coffee.CAPPUCCINO;
+        System.out.println(food);
+    }
+}
